cfad53b3eb676a175f4d69d86b3903f821356562
yishai <yishai5555@gmail.com>
2019-08-13 15:35:11 +0300

    b4

--- aws-security-groups.tf
+++ aws-security-groups.tf
@@ -23,6 +23,14 @@ resource "aws_security_group" "k8s-linuxacademy-terraform-project-sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "http"
   }
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "http"
+  }
  
   ingress {
     from_port   = 443
